1d8bfaf62508bf1c809218a83344a790d92f822b
Benjamin Layet <layetbenjamin@gmail.com>
2026-01-30 15:33:11 +0100

    removes base url "meditation-reso"

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,6 @@ import packageJson from "./package.json" with { type: "json" };
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // Set base path for GitHub Pages
-  base: "/meditation-reso/",
-
   server: {
     open: true,
   },
