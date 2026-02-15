cfa41327cc108ff29b492e8c0ae6e52c494aef0e
Benjamin Layet <layetbenjamin@gmail.com>
2026-02-15 21:36:40 +0100

    adds mobile os code to version

--- src/util/version.ts
+++ src/util/version.ts
@@ -1,8 +1,10 @@
 import packageJson from "../../package.json";
 import buildInfoJson from "../../build-info.json";
+import { isAndroidOnWebView } from "./useragent.ts";
 const localizedBuildInfo = {
   version: packageJson.version,
   ...buildInfoJson,
   builtAt: new Date(buildInfoJson.builtAt).toLocaleString(),
+  mobileOs: isAndroidOnWebView() ? "A" : "I",
 };
 export const VERSION = Object.values(localizedBuildInfo).join("-");
