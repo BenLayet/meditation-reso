664fddd9ea3e65680613f71e498d7f38db2aaf6b
Benjamin Layet <layetbenjamin@gmail.com>
2026-02-15 14:07:31 +0100

    DEBUG #3

--- src/services/gong.service.ts
+++ src/services/gong.service.ts
@@ -1,30 +1,9 @@
-import gongSound from "../assets/gong.mp3";
-
 export class GongService {
-  private beginningAudio = new Audio(gongSound);
-  private endAudio = new Audio(gongSound);
+  loadAudio = () => {};
 
-  loadAudio = () => {
-    this.beginningAudio.load();
-    this.endAudio.load();
-  };
+  playBeginningAudio = () => {};
 
-  playBeginningAudio = () => {
-    this.beginningAudio.currentTime = 0;
-    this.beginningAudio.play().catch((err: unknown) => {
-      console.error("Audio play failed:", err);
-    });
-  };
+  playEndAudio = () => {};
 
-  playEndAudio = () => {
-    this.endAudio.currentTime = 0;
-    this.endAudio.play().catch((err: unknown) => {
-      console.error("Audio play failed:", err);
-    });
-  };
-
-  stopAllAudio = () => {
-    this.beginningAudio.pause();
-    this.endAudio.pause();
-  };
+  stopAllAudio = () => {};
 }
